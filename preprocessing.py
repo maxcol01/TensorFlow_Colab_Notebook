4cb867ad00112ed9a278ebe9bbdef16d6659c574
maxcol01 <159482922+maxcol01@users.noreply.github.com>
2024-10-18 16:50:35 +0200

    Add files via upload

--- preprocessing.py
+++ preprocessing.py
@@ -28,7 +28,7 @@ def cast_into_dict(filename) -> list:
             test_dico["line_number"] = counter
             test_dico["target"] = item[0]
             test_dico["text"] = item[1].strip("\n").lower()
-            test_dico["total_lines"] = length-1
+            test_dico["total_lines"] = length
             list_dico.append(test_dico.copy())
             counter += 1
 
